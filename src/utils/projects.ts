bf4c5b1a3edaf67d07466645198be36450cd4471
cihanerenler <cihanerenler@outlook.com>
2024-01-16 03:13:26 +0200

    fix image not show up

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,9 +1,16 @@
+import portfolioApp from "../assets/images/portfolio-app.jpg";
+import ecommerce from "../assets/images/e-commerce.jpg";
+import hero from "../assets/images/hero.png";
+import project4 from "../assets/images/project-4.png";
+import project2 from "../assets/images/project-2.jpg";
+import project3 from "../assets/images/project-3.jpg";
+
 const projects = [
   {
     id: 0,
     title: "Portfolio Website",
     desc: "This project involves the creation of my personal website, which you are currently exploring. The primary goal is to provide visitors with information about me and showcase various projects I have worked on.",
-    image: "/src/assets/images/portfolio-app.jpg",
+    image: portfolioApp,
     github: "https://github.com/CihanErenler/portfolio-app-v2",
     preview: "https://hanci.netlify.app/",
     tags: ["TypeScript", "React", "Tailwind CSS", "React Router"],
@@ -20,7 +27,7 @@ const projects = [
     id: 1,
     title: "E-commerce Website",
     desc: "The E-commerce Website project involves the development of an online shopping platform where users can filter available products based on various criteria such as brand, color, and price. Users can also preview individual products and add them to the shopping cart.",
-    image: "/src/assets/images/e-commerce.jpg",
+    image: ecommerce,
     github: "https://github.com/CihanErenler/react-e-commerce",
     preview: "https://hanci.netlify.app/",
     tags: ["React", "Tailwind Css", "React Router", "Axios", "React Icons"],
@@ -43,7 +50,7 @@ const projects = [
     id: 2,
     title: "Markdown Note Taking Application",
     desc: "NoteDock is a dynamic markdown note-taking application that seamlessly combines the simplicity of markdown with the power of a feature-rich note management system.",
-    image: "/src/assets/images/hero.png",
+    image: hero,
     github: "https://github.com/CihanErenler/markdown-note-taking-offline",
     preview: "https://notedockio.netlify.app",
     tags: ["React", "Styled-components", "React Router"],
@@ -60,7 +67,7 @@ const projects = [
     id: 3,
     title: "Movie Search App",
     desc: "The Movie Search App is designed to allow users to display trending movies and TV shows, view details of specific films, and search for movies using queries.",
-    image: "/src/assets/images/project-4.png",
+    image: project4,
     github: "https://github.com/CihanErenler/react-movie-app",
     preview: "https://cihan-react-movieapp.netlify.app/",
     tags: ["React", "CSS", "React Router", "Material UI"],
@@ -77,7 +84,7 @@ const projects = [
     id: 4,
     title: "Color Generator",
     desc: "The Color Generator project involves the creation of a React tool that allows users to generate tints and shades from a selected color. Additionally, the tool enables users to copy any generated color.",
-    image: "/src/assets/images/project-2.jpg",
+    image: project2,
     github: "https://github.com/CihanErenler/react-color-generator",
     preview: "https://c-react-color-generator.netlify.app/",
     tags: ["React", "CSS", "React Color", "React Icons"],
@@ -94,7 +101,7 @@ const projects = [
     id: 5,
     title: "Weather App",
     desc: "The Weather App is designed to allow users to search for real-time weather conditions in different cities. Users can retrieve information such as temperature, humidity, and pressure.",
-    image: "/src/assets/images/project-3.jpg",
+    image: project3,
     github: "https://github.com/CihanErenler/weatherApp",
     preview: "https://cihanweatherapp.netlify.app/",
     tags: ["Javascript", "CSS"],
